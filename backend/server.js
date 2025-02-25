abfcbc996ee36b8ca34a3cad0afa8da48ee401b6
developernajib <developernajib@gmail.com>
2025-02-25 14:54:07 +0600

    backend setup add

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import products from './data/products.js';
+import dotenv from 'dotenv';
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello from E-commerce server!');
+});
+
+app.get('/api/products', (req, res) => {
+    res.json(products);
+});
+
+app.get('/api/products/:id', (req, res) => {
+    const product = products.find((p) => p._id === req.params.id);
+    if (product) {
+        res.json(product);
+    } else {
+        res.status(404).json({ message: 'Product Not Found' });
+    }
+});
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
+console.log(`App available at http://localhost:${port}`);
+
