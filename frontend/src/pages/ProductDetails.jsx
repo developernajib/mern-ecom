8dcf984041ba35c44a6403f3e9ad5680d17af777
developernajib <developernajib@gmail.com>
2025-02-26 14:53:29 +0600

    product fetch from api

--- frontend/src/pages/ProductDetails.jsx
+++ frontend/src/pages/ProductDetails.jsx
@@ -1,16 +1,24 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import products from "../products";
 import { Link } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
 import Rating from "../components/Rating";
+import axios from "axios";
 
 const ProductDetails = () => {
-	const { id: productId } = useParams();
-	const product = products.find((p) => p._id === productId);
 
-	if (!product) {
-		return <div>Product not found</div>;
-	}
+	const { id: productId } = useParams();
+
+	const [product, setProduct] = useState([]);
+
+	useEffect(() => {
+		const fetchProduct = async () => {
+			const { data } = await axios.get(`/api/products/${productId}`);
+			setProduct(data);
+		};
+
+		fetchProduct();
+	}, []);
 
 	return (
 		<>
