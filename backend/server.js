04ee4b8ac1fe9190c740ad5a558ca46c25b2c2fe
developernajib <developernajib@gmail.com>
2025-02-27 14:04:17 +0600

    data fetch from mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from 'express';
-import products from './data/products.js';
-
+import productRoutes from './routes/productRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -16,18 +16,10 @@ app.get('/', (req, res) => {
     res.send('Hello from E-commerce server!');
 });
 
-app.get('/api/products', (req, res) => {
-    res.json(products);
-});
+app.use('/api/products', productRoutes);
 
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((p) => p._id === req.params.id);
-    if (product) {
-        res.json(product);
-    } else {
-        res.status(404).json({ message: 'Product Not Found' });
-    }
-});
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
 console.log(`App available at http://localhost:${port}`);
